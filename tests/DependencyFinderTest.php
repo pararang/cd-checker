c02d206f7877d4b34906bdee37df25283533f11c
Kevin Kaufmann <KaufmannKevin@outlook.de>
2019-10-03 11:17:29 +0200

    test RuntimeException for false SplFileInfo

---
+++ tests/DependencyFinderTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Selective\CdChecker\Test;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use RuntimeException;
+use Selective\CdChecker\DependencyFinder;
+use PHPUnit\Framework\TestCase;
+use SplFileInfo;
+
+class DependencyFinderTest extends TestCase
+{
+    /**
+     * @var DependencyFinder $dependencyFinder
+     */
+    private $dependencyFinder;
+
+    public function setUp()
+    {
+        $this->dependencyFinder = new DependencyFinder();
+    }
+
+    public function testProcessFileWithSplFileInfoFalseExpectRuntimeException()
+    {
+        $this->expectException(RuntimeException::class);
+
+        /** @var SplFileInfo|MockObject $splFileInfo */
+        $splFileInfo = $this->createMock(SplFileInfo::class);
+        $splFileInfo->method("getRealPath")
+            ->willReturn(false);
+
+        $this->dependencyFinder->processFile($splFileInfo, []);
+    }
+}
